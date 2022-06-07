b748d452457a353bfe5cf05c12d85803ac2b8f81
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-06-07 11:09:50 -0400

    working with math in c#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TheTriangleFarmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double triangleHeight;
+            double triangleBase;
+
+            Console.WriteLine("What is the triangle's Base?");
+            triangleBase = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is the triangle's Height?");
+            triangleHeight = Convert.ToDouble(Console.ReadLine());
+
+            double area = triangleBase * triangleHeight / 2;
+
+            Console.WriteLine("The triangle's Area is: " + area);
+
+        }
+    }
+}
